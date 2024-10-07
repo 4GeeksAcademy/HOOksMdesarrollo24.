01cded329166c3f25640e55af25efd9a9414d953
mdesarrollo24 <mar.1298ch@gmail.com>
2024-10-07 23:39:25 +0000

    ok

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,24 @@
+import React from "react"; // Import React
+import "../../styles/index.css"; // Import CSS file
+
+// Component to traffi light
+const TrafficLight = ({ color, onColorChange }) => { 
+  return (
+    <div className="traffic-light">
+      <div
+        className={`circle circle-red ${color === "red" ? "turn-on" : ""}`}
+        onClick={() => onColorChange("red")} 
+      ></div>
+      <div
+        className={`circle circle-orange ${color === "orange" ? "turn-on" : ""}`}
+        onClick={() => onColorChange("orange")} 
+      ></div>
+      <div
+        className={`circle circle-green ${color === "green" ? "turn-on" : ""}`}
+        onClick={() => onColorChange("green")} 
+      ></div>
+    </div>
+  );
+};
+
+export default TrafficLight; // Export TrafficLight
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,21 @@
-import React from "react";
+import React, { useState } from "react"; // Import React and useState
+import "../../styles/index.css"; // Import CSS
+import TrafficLight from "./TrafficLight"; // Import component TrafficLight
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+// Component principal
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [color, setColor] = useState("red"); // Save actual state
+
+  const handleColorChange = (selectedColor) => {
+    setColor(selectedColor); // Change color with a click
+  };
+
+  return (
+    <div className="text-center">
+      <h1>Semáforo</h1> {/* Título del semáforo */}
+      <TrafficLight color={color} onColorChange={handleColorChange} /> 
+    </div>
+  );
 };
 
-export default Home;
+export default Home; // Export component Home
